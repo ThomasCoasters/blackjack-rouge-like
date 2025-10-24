fe35beb788b081f22122c28fe4570ca939e2e633
ThomasCoasters <ThomasTerlouw888@gmail.com>
2025-10-24 20:22:45 +0200

    bonus info text + better looking info text

--- js/cards/card_hover.js
+++ js/cards/card_hover.js
@@ -1,4 +1,4 @@
-function show_Card_Info(card, name, text) {
+function show_Card_Info(card, name, text, card_raw = null) {
     const infoBox = document.createElement("div");
     infoBox.className = "card-info-box";
 
@@ -15,12 +15,19 @@ function show_Card_Info(card, name, text) {
     infoBox.style.left = (rect.left - 100) + "px"; // center the box
 
     document.body.appendChild(infoBox);
+
+    look_for_special_hover_texts(card, card_raw, infoBox);
 }
 
 function hide_Card_Info() {
     const infoBoxes = document.getElementsByClassName("card-info-box");
     while (infoBoxes[0]) {
         infoBoxes[0].parentNode.removeChild(infoBoxes[0]);
+    }
+
+    const specialBoxes = document.getElementsByClassName("card-special-box");
+    while (specialBoxes[0]) {
+        specialBoxes[0].parentNode.removeChild(specialBoxes[0]);
     }
 }
 
@@ -36,5 +43,62 @@ function text_color_change(text) {
     new_text = new_text.replace(/(face|jack|queen|king)/g, '<span style="color: lightgreen;">$1</span>');
     new_text = new_text.replace(/(card)/g, '<span style="color: green;">$1</span>');
 
+    new_text = new_text.replace(/(retrigger)/g, '<span style="color: green;">$1</span>');
+
     return new_text;
+}
+
+
+
+
+
+function look_for_special_hover_texts(card, card_raw, infoBox) {
+    if (!card_raw) return;
+
+
+    let specials = [];
+    let specials_names = [];
+
+    if (card_raw.retrigger > 0) {
+        specials_names.push("retrigger");
+        // card_raw.retrigger = 2; // temp for testing
+        specials.push("This card can retrigger" + (" up to " + card_raw.retrigger + " times."));
+    }
+
+    if (card_raw.different_value_and_score === true) {
+        specials_names.push("score difference");
+        specials.push("this card can have a different value and score.");
+    }
+
+    if (card_raw.reusing === true) {
+        specials_names.push("reusable");
+        specials.push("this card can be reused until you bust or win.");
+    }
+
+    if (specials.length > 0) {
+        special_hover_texts(specials, specials_names, card, infoBox);
+    }
+}
+
+
+function special_hover_texts(specials, specials_names, card, infoBox) {
+    const specialBox = document.createElement("div");
+    specialBox.className = "card-special-box";
+
+    for (let i = 0; i < specials.length; i++) {
+        const coloredName = text_color_change(specials_names[i]);
+        const coloredText = text_color_change(specials[i]);
+
+        specialBox.innerHTML += `<strong>${coloredName}</strong><br><p>${coloredText}</p>`;
+    }
+
+    // Position the special box
+    const cardRect = card.getBoundingClientRect();
+    const infoRect = infoBox.getBoundingClientRect();
+
+
+    specialBox.style.top = (infoRect.top + infoRect.height/2) + "px";
+    specialBox.style.left = (cardRect.left - 100 + infoRect.width) + "px";
+
+    document.body.appendChild(specialBox);
 }
