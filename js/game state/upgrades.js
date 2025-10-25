fe5c58f3e2e816a45fafad0c2a502e41d1cba56a
ThomasCoasters <ThomasTerlouw888@gmail.com>
2025-10-25 19:51:09 +0200

    upgrade rebalancing + bug fixing

--- js/game state/upgrades.js
+++ js/game state/upgrades.js
@@ -15,10 +15,10 @@ const all_upgrades = {
 
     "permanent_upgrades": [
     {"suit": "permanent_upgrade_row_1", "value": "increase_max_total_value", "hover_name": "increase max total value", "hover_text": "increases the maximum total value by 2", "effect": increase_max_total_value},
-    {"suit": "permanent_upgrade_row_1", "value": "increase_max_total_value_but_draw_more", "hover_name": "increase max total value", "hover_text": "increases the maximum total value by 5 but draw 1 more card at the beginning", "effect": increase_max_total_value_but_draw_more},
+    {"suit": "permanent_upgrade_row_1", "value": "increase_max_total_value_but_less_discards", "hover_name": "increase max total value", "hover_text": "increases the maximum total value by 5 but have 1 less discard", "effect": increase_max_total_value_but_less_discards},
     {"suit": "permanent_upgrade_row_1", "value": "increase_blackjack_bonus", "hover_name": "increase blackjack bonus", "hover_text": "increases the blackjack bonus multiplier by 0.5", "effect": increase_blackjack_bonus},
     {"suit": "permanent_upgrade_row_1", "value": "extra_upgrade_slot_but_decrease_max_total_value", "hover_name": "extra upgrade slot", "hover_text": "increases the amount of upgrades you can choose from by 1 but maximum total value decreases by 2", "effect": extra_upgrade_slot_but_decrease_max_total_value},
-    {"suit": "permanent_upgrade_row_1", "value": "increase_hands_but_negative_blackjack", "hover_name": "extra hands", "hover_text": "increases number of hands by 1 after 1 round but decrease blackjack multiplier to -0.5 (losing points for blackjack)", "effect": increase_hands_but_negative_blackjack},
+    {"suit": "permanent_upgrade_row_1", "value": "increase_hands_but_negative_blackjack", "hover_name": "extra hands", "hover_text": "increases number of hands by 1 but decrease blackjack multiplier to -0.5 (losing points for blackjack)", "effect": increase_hands_but_negative_blackjack},
     ],
 
 
@@ -45,7 +45,7 @@ const col_map = {
     "special_value:5": 0,
 
     "increase_max_total_value": 0,
-    "increase_max_total_value_but_draw_more": 0,
+    "increase_max_total_value_but_less_discards": 0,
     "increase_blackjack_bonus": 0,
     "extra_upgrade_slot_but_decrease_max_total_value": 0,
     "increase_hands_but_negative_blackjack": 0,
@@ -167,9 +167,9 @@ async function increase_max_total_value() {
     window.max_total_value += 2;
 }
 
-async function increase_max_total_value_but_draw_more() {
+async function increase_max_total_value_but_less_discards() {
     window.max_total_value += 5;
-    window.forced_amount_draw += 1;
+    window.max_discards_amount -= 1;
 }
 
 async function increase_blackjack_bonus() {
@@ -182,6 +182,7 @@ async function extra_upgrade_slot_but_decrease_max_total_value() {
 }
 
 async function increase_hands_but_negative_blackjack() {
+    window.hands_amount += 1;
     window.max_hands_amount += 1;
     window.blackjack_bonus_multiplier = -0.5;
 }
